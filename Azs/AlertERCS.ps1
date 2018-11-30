3b8db3f229b090517f8310b6d717a985860d891b
troettinger <thoroet@microsoft.com>
2018-11-30 22:44:27 +0100

    ERCS monitoring with Email

---
+++ Azs/AlertERCS.ps1
@@ -0,0 +1,34 @@
+﻿$FQDN = "azurestack.external"
+$RegionName = "local"
+
+#Set Parameter
+$secpasswd = ConvertTo-SecureString "EmailAccountPassword" -AsPlainText -Force
+$emailcredential = New-Object System.Management.Automation.PSCredential ("EmailAccountUserName", $secpasswd)
+$From = "SenderEmailAddress"
+$To = "ReceiptEmailAddress"
+$Subject = "Azure Stack ERCS Alert"
+$SMTPServer = "MailServer"
+$SMTPPort = "587"
+
+#Add & Authenticate environment
+Add-AzureRmEnvironment -Name Admin -ARMEndpoint https://adminmanagement.$regionname.$FQDN |out-null
+Add-AzureRmAccount -Environment Admin|Out-Null
+
+$Alerts=Get-AzsAlert | where {($_.State -eq "Active" -and $_.Title -eq "Infrastructure role instance unavailable")}|select ImpactedResourceDisplayName
+
+
+Foreach ($alert in $alerts){
+
+If ($alert |where {$_.ImpactedResourceDisplayName -like "*ERCS*"})
+
+{
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -Credential $emailcredential -UseSsl
+}
+}
+
+
+
+
+
+
+
