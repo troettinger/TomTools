a052c83bc8bdc850a283e0190370abfb199321d9
troettinger <thoroet@microsoft.com>
2018-12-01 02:49:01 +0100

    Adding more Sample

---
+++ Azs/showapiversions.ps1
@@ -0,0 +1,11 @@
+﻿$FQDN = Read-Host "Enter External FQDN"
+$RegionName = Read-Host "Enter Azure Stack Region Name"
+Add-AzureRmEnvironment -Name Admin -ARMEndpoint https://management.$regionname.$FQDN |out-null
+Add-AzureRmAccount -Environment Admin|Out-Null
+
+
+Get-AzureRmResourceProvider | `
+  Select ProviderNamespace -Expand ResourceTypes | `
+  Select * -Expand ApiVersions | `
+  Select ProviderNamespace, ResourceTypeName, @{Name="ApiVersion"; Expression={$_}}
+  
